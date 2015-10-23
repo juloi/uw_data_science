5549ebe2a8f16fce8e4691d0b89d77985daa0bb2
Julio Salvador Venegas Garcia <juloi@Julios-MacBook-Air.local>
2015-10-23 16:35:55 +0200

    final one

--- assignment3/data/asymmetric_friendships.py
+++ assignment3/data/asymmetric_friendships.py
@@ -5,18 +5,42 @@ from pprint import pprint as pp
 mr = MapReduce.MapReduce()
 
 def mapper(record):
-    mr.emit_intermediate((record[1], record[0]), (record[0], record[1]))
+    # mr.emit_intermediate((record[0], record[1]), 1)
+    # mr.emit_intermediate((record[1], record[0]), -1)
+
+    mr.emit_intermediate((record[0], record[1]), (record[1], record[0]))
 
 def reducer(key, list_of_values):
-    friendships = {}
-    friendships[key] = list_of_values[0]
+    counter = 0
+    for value in list_of_values:
+        counter += value
 
-    for key, val in friendships.iteritems():
-        try:
-            friendships[val]
-        except KeyError:
-            mr.emit(key)
-            mr.emit(val)
+    if counter < 0:
+        mr.emit(key)
+        mr.emit((key[1], key[0]))
+
+    # friendships = {}
+    # friendships[key] = list_of_values[0]
+    # pp(friendships)
+
+    # inversed_values = []
+    # for key in friendships.iterkeys():
+        # inversed_values.append(friendships[key][0])
+        # pp(inversed_values)
+
+    # for val in friendships.itervalues():
+    #     if val not in friendships:
+    #         pp(val)
+        # else:
+        #     mr.emit(val)
+
+    # for key, val in friendships.iteritems():
+    #     try:
+    #         friendships[val]
+    #     except KeyError:
+    #         continue
+            # mr.emit(key)
+            # mr.emit(val)
 
 data = open(sys.argv[1], 'r')
 
