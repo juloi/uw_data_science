9886bd7579ac7a6be723af62530cd3173f94040b
Julio Salvador Venegas Garcia <juloi@Julios-MacBook-Air.local>
2015-10-23 14:43:03 +0200

    try 1 ready

---
+++ assignment3/data/asymmetric_friendships.py
@@ -0,0 +1,22 @@
+import MapReduce
+import sys
+from pprint import pprint as pp
+
+mr = MapReduce.MapReduce()
+
+def mapper(record):
+    mr.emit_intermediate((record[0], record[1]), (record[1], record[0]))
+
+def reducer(key, list_of_values):
+    friendships = {}
+    friendships[key] = list_of_values[0]
+
+    for val in friendships.itervalues():
+        try:
+            friendships[val]
+        except KeyError:
+            mr.emit(val)
+
+data = open(sys.argv[1], 'r')
+
+mr.execute(data, mapper, reducer)
